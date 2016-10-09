1d7fbaf15d0bc5f0b8455fdc2d5c27746cac4762
James Clark <sbj@dimins.com>
2016-10-09 16:45:14 -0400

    Add comments

--- src/generate.js
+++ src/generate.js
@@ -1,9 +1,28 @@
 var Maze = require('@sbj42/maze-generator-support').Maze;
 
 function generate(width, height, options) {
-    options = options || {};
-    options.random = options.random || Math.random;
-    options.generator = options.generator || '@sbj42/maze-generator-backtrack';
+    var noptions = {};
+    for (var x in options)
+        noptions[x] = options[x];
+
+    // Default random number generator: Math.random.
+    // Math.random isn't seedable, so if you want reproducible
+    // results you should pass a different RNG, for instance
+    // the following code using the random-js package would work:
+    //
+    //     var randomjs = require('random-js');
+    //     var engine = randomjs.engines.mt19937().seed(seed);
+    //     var real = randomjs.real(0, 1);
+    //     options.random = function() {
+    //         return real(engine);
+    //     };
+    if (!noptions.random)
+        options.random = Math.random;
+
+    // Default generator.  maze-generator-backtrack isn't included
+    // in this library, it needs to be installed separately.
+    if (!options.generator)
+        options.generator = '@sbj42/maze-generator-backtrack';
 
     var maze = new Maze(width, height);
 
